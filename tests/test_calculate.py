e86a90b346b3d64bce81803b9728dc528e5648f8
Artem Iushin <119945340+ArtemIushin123@users.noreply.github.com>
2024-11-14 19:25:57 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -3,10 +3,7 @@ from Programs.calculate import calc
 import sys
 import os
 import pytest
-sys.path.insert(0, os.path.abspath\
-    (os.path.join(os.path.dirname(__file__), '..', 'Programs')))
-
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Programs')))
 
 class TestCalc:
     @pytest.mark.parametrize(
